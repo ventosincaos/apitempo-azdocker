d21854984554edd782f09c83854dead567094614
ventosincaos <111576277+ventosincaos@users.noreply.github.com>
2025-02-03 17:46:05 -0300

    primeiro commit

---
+++ API/WeatherForecast.cs
@@ -0,0 +1,14 @@
+namespace API;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public int Temperaturek => 273 + TemperatureC;
+
+    public string? Summary { get; set; }
+}
